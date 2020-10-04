72e482925e22690a67b50ca8718849399064609d
Sehajpreet Singh <47858754+sehaj01@users.noreply.github.com>
2020-10-04 17:20:42 +0530

    Delete combobox.py

--- combobox.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from tkinter.ttk import Combobox
-window=Tk()
-var = StringVar()
-var.set("one")
-data=("one", "two", "three", "four")
-cb=Combobox(window, values=data)
-cb.place(x=60, y=150)
